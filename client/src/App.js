011d5b6f504ccbce13d648a7746b5131ec290801
saivamsi1911 <sv191000@gmail.com>
2021-03-28 15:10:56 +0530

    small change

--- client/src/App.js
+++ client/src/App.js
@@ -14,28 +14,28 @@ import AlertState from './context/alert/AlertState';
 import './App.css';
 
 const App = () => {
-  return (
-    <AuthState>
-      <ContactState>
-        <AlertState>
-          <Router>
-            <Fragment>
-              <Navbar />
-              <div className='container'>
-                <Alerts />
-                <Switch>
-                  <PrivateRoute exact path='/' component={Home} />
-                  <Route exact path='/about' component={About} />
-                  <Route exact path='/register' component={Register} />
-                  <Route exact path='/login' component={Login} />
-                </Switch>
-              </div>
-            </Fragment>
-          </Router>
-        </AlertState>
-      </ContactState>
-    </AuthState>
-  );
+	return (
+		<AuthState>
+			<ContactState>
+				<AlertState>
+					<Router>
+						<Fragment>
+							<Navbar />
+							<div className='container'>
+								<Alerts />
+								<Switch>
+									<PrivateRoute exact path='/' component={Home} />
+									<Route exact path='/about' component={About} />
+									<Route exact path='/register' component={Register} />
+									<Route exact path='/login' component={Login} />
+								</Switch>
+							</div>
+						</Fragment>
+					</Router>
+				</AlertState>
+			</ContactState>
+		</AuthState>
+	);
 };
 
 export default App;
